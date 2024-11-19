46642f1abe6cbdc2022f366e1f139b97e09f59f1
Jalen <ja1897@uncw.edu>
2024-11-19 13:21:21 -0500

    Achievements
    Achievements skeleton
    work in progress

---
+++ VP_Simulator/src/main/java/com/example/vp_simulator/AchievementController.java
@@ -0,0 +1,75 @@
+package com.example.vp_simulator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+public class Achievement {
+
+    @FXML
+    private ListView<String> achievementsList;
+
+    @FXML
+    private GridPane achievementGridPane;
+
+    @FXML
+    private Button allButton;
+
+    @FXML
+    private Label bathroomLabel;
+
+    @FXML
+    private TextArea bathroomTextArea;
+
+    @FXML
+    private Label bdayLabel;
+
+    @FXML
+    private TextArea bdayTextArea;
+
+    @FXML
+    private Button bmmButton;
+
+    @FXML
+    private Label firstwalkLabel;
+
+    @FXML
+    private TextArea firstwalkTextArea;
+
+    @FXML
+    private Label hungerLabel;
+
+    @FXML
+    private TextArea hungerTextArea;
+
+    @FXML
+    private Button miscButtons;
+
+    @FXML
+    private TextArea nightTextArea;
+
+    @FXML
+    private Button petfeatsButton;
+
+    @FXML
+    private ScrollBar scrollBar;
+
+    @FXML
+    private Label sleepLabel;
+
+    @FXML
+    private Label tdLabel;
+
+    @FXML
+    private TextArea trainingdayTextArea;
+
+    @FXML
+    private Label vvLabel;
+
+    @FXML
+    private TextArea vvTextArea;
+
+   
+}
+
