38b10bd5013f2d546a339c47645792ff4eb6e7f6
Jalen <ja1897@uncw.edu>
2024-11-21 12:25:46 -0500

    Achievements
    ~Created the scrollPane to allow the user to scroll up and down prohibits left and right movements
    ~Created unlock and locked achievements that should allow the row for the achievement that is unlocked to display as brown with white text color and the locked as grey with white text color
    (Not sure if any of this works)

--- VP_Simulator/src/main/java/com/example/vp_simulator/AchievementController.java
+++ VP_Simulator/src/main/java/com/example/vp_simulator/AchievementController.java
@@ -1,11 +1,41 @@
 package com.example.vp_simulator;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import org.w3c.dom.Node;
+import org.w3c.dom.Text;
 
-public class Achievement {
+import javax.swing.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AchievementController extends VP_Simulator {
+    enum isUnlocked {Locked, Unlocked};
+
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Achievements.fxml"));
+
+        ScrollPane scrollpane = new ScrollPane();
+        scrollpane.setContent(root);
+
+        scrollpane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scrollpane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Pet Achievements");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
     private ListView<String> achievementsList;
@@ -70,6 +100,52 @@ public class Achievement {
     @FXML
     private TextArea vvTextArea;
 
-   
+    @FXML
+    private void allButtonAction() {
+        System.out.println("All Button Pressed: ");
+
+    }
+
+    @FXML
+    private void petfeatsButtonAction() {
+        System.out.println("Pet Feats Button Pressed: ");
+    }
+
+    @FXML
+    private void miscButtonAction() {
+        System.out.println("Miscellaneous Button Pressed: ");
+    }
+
+    @FXML
+    private void bmmButtonAction() {
+        Stage stage = (Stage) bmmButton.getScene().getWindow();
+        System.out.println("Back to Main Menu Pressed: ");
+    }
+
+    public void unlockAchivement(isUnlocked status) {
+        if (status == isUnlocked.Unlocked) {
+            achievementGridPane.getChildren().forEach(node -> {
+                Integer rowIndex = GridPane.getRowIndex(node);
+                if (rowIndex != null && status == isUnlocked.Unlocked) {
+                    node.setEffect(new DropShadow(10, Color.WHITE));
+
+                    node.setStyle("-fx-background-color: brown;");
+                }
+            });
+
+        }
+    }
+    public void lockedAchievement(isUnlocked status){
+        if (status == isUnlocked.Locked){
+            achievementGridPane.getChildren().forEach(node -> {
+                Integer rowIndex = GridPane.getRowIndex(node);
+                if (rowIndex != null && status == isUnlocked.Locked){
+                    node.setEffect(new DropShadow(10, Color.WHITE));
+
+                    node.setStyle("-fx-background-color: grey;");
+                }
+            });
+        }
+    }
 }
 
